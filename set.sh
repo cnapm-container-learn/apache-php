af56ee893aee88ca5529bdb456debc717f578502
wdxxs2z <243219650@qq.com>
2015-12-25 00:17:13 +0800

    Fix default apache conf.d

--- set.sh
+++ set.sh
@@ -3,4 +3,12 @@
 sed -i "s#^nbs.license_key =.*#nbs.license_key = \"${TINGYUN_LICENSE_KEY}\"#g" /etc/php5/cli/conf.d/networkbench.ini
 sed -i "s#^nbs.app_name=.*#nbs.app_name = \"${TINGYUN_APP_NAME}\"#g" /etc/php5/cli/conf.d/networkbench.ini
 
+sed -i "s#^nbs.license_key =.*#nbs.license_key = \"${TINGYUN_LICENSE_KEY}\"#g" /etc/php5/fpm/conf.d/networkbench.ini
+sed -i "s#^nbs.app_name=.*#nbs.app_name = \"${TINGYUN_APP_NAME}\"#g" /etc/php5/fpm/conf.d/networkbench.ini
+
+cp /etc/php5/fpm/conf.d/networkbench.ini /etc/php5/apache2/conf.d/
+
+echo "nbs.license_key=${TINGYUN_LICENSE_KEY}" >> /var/run/networkbench/.networkbench.cfg
+echo "nbs.app_name=${TINGYUN_APP_NAME}" >> /var/run/networkbench/.networkbench.cfg
+
 /etc/init.d/php5-fpm start
