43254e8ac495e7ecf235a03c63bb2fa58b5ada2c
wdxxs2z <243219650@qq.com>
2015-12-25 16:30:48 +0800

    Add oneapm.

---
+++ tingyun/set.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function modify_file() {
+	sed -i "s#^nbs.license_key =.*#nbs.license_key = \"${TINGYUN_LICENSE_KEY}\"#g" $1
+    sed -i "s#^nbs.app_name=.*#nbs.app_name = \"${TINGYUN_APP_NAME}\"#g" $1
+}
+
+if [ -f /etc/php5/fpm/conf.d/networkbench.ini ]; then
+	modify_file /etc/php5/fpm/conf.d/networkbench.ini
+	/etc/init.d/php5-fpm start
+fi
+
+if [ -f /etc/php5/apache2/conf.d/networkbench.ini ]; then
+	modify_file /etc/php5/apache2/conf.d/networkbench.ini
+else
+	if [ -f /etc/php5/cli/conf.d/networkbench.ini ]; then
+		cp /etc/php5/cli/conf.d/networkbench.ini /etc/php5/apache2/conf.d/
+		modify_file /etc/php5/apache2/conf.d/networkbench.ini
+	else
+		echo "The networkbench.ini has not in PHP environment."
+	fi
+fi
