1bca1fef04a9d8592d53e7853757a86706650394
wdxxs2z <243219650@qq.com>
2015-12-25 11:31:34 +0800

    Remove php5-fpm.

--- set.sh
+++ set.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-sed -i "s#^nbs.license_key =.*#nbs.license_key = \"${TINGYUN_LICENSE_KEY}\"#g" /etc/php5/cli/conf.d/networkbench.ini
-sed -i "s#^nbs.app_name=.*#nbs.app_name = \"${TINGYUN_APP_NAME}\"#g" /etc/php5/cli/conf.d/networkbench.ini
+if [ -f /etc/php5/fpm/conf.d/networkbench.ini ]; then
+	modify_file /etc/php5/fpm/conf.d/networkbench.ini
+	/etc/init.d/php5-fpm start
+fi
 
-sed -i "s#^nbs.license_key =.*#nbs.license_key = \"${TINGYUN_LICENSE_KEY}\"#g" /etc/php5/fpm/conf.d/networkbench.ini
-sed -i "s#^nbs.app_name=.*#nbs.app_name = \"${TINGYUN_APP_NAME}\"#g" /etc/php5/fpm/conf.d/networkbench.ini
+if [ -f /etc/php5/apache2/conf.d/networkbench.ini ]; then
+	modify_file /etc/php5/apache2/conf.d/networkbench.ini
+else
+	if [ -f /etc/php5/cli/conf.d/networkbench.ini ]; then
+		cp /etc/php5/cli/conf.d/networkbench.ini /etc/php5/apache2/conf.d/
+		modify_file /etc/php5/apache2/conf.d/networkbench.ini
+	else
+		echo "The networkbench.ini has not in PHP environment."
+	fi
+fi
 
-cp /etc/php5/fpm/conf.d/networkbench.ini /etc/php5/apache2/conf.d/
-
-echo "nbs.license_key=${TINGYUN_LICENSE_KEY}" >> /var/run/networkbench/.networkbench.cfg
-echo "nbs.app_name=${TINGYUN_APP_NAME}" >> /var/run/networkbench/.networkbench.cfg
-
-/etc/init.d/php5-fpm start
+function modify_file() {
+	sed -i "s#^nbs.license_key =.*#nbs.license_key = \"${TINGYUN_LICENSE_KEY}\"#g" $1
+    sed -i "s#^nbs.app_name=.*#nbs.app_name = \"${TINGYUN_APP_NAME}\"#g" $1
+}
